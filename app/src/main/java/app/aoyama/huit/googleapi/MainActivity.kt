f2e25a26e8c442daa7c1d4af33d42e3671ef6b01
Huit-8 <eitoao88@gmail.com>
2022-09-20 23:51:26 +0900

    first commit

---
+++ app/src/main/java/app/aoyama/huit/googleapi/MainActivity.kt
@@ -0,0 +1,25 @@
+package app.aoyama.huit.googleapi
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
